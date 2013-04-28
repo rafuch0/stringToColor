407cf18c28967968b603a60e416186e6c2317401
localhost <root@localhost>
2013-04-28 09:35:50 -0400

    null

---
+++ stringToColor.js
@@ -0,0 +1,39 @@
+function colorCode(str)
+{
+	var r = 128;
+	var g = 64;
+	var b = 32;
+
+	var front;
+	var back;
+
+	if(str)
+	{
+		for(var i = 0, maxi = str.length; i < maxi; i++)
+		{
+			front = str.charCodeAt(i);
+			back = str.charCodeAt(str.length - 1 - i);
+			r = r + (Math.pow(front % 16, 2) + back) * (i * 4) - Math.pow((back + i) % 16, 2);
+			g = g + (Math.pow(front % 16, 2) + back) * (i * 3) - Math.pow((back + i) % 16, 2);
+			b = b + (Math.pow(front % 16, 2) + back) * (i * 2) - Math.pow((back + i) % 16, 2);
+		}
+	}
+
+	r = (r >= 0) ? r : -1*r;
+	g = (g >= 0) ? g : -1*g;
+	b = (b >= 0) ? b : -1*b;
+
+	r = (r + g + 32) % 256;
+	g = (g + b + 64) % 256;
+	b = (b + r + 128) % 256;
+
+	r = r.toString(16);
+	g = g.toString(16);
+	b = b.toString(16);
+
+	if(r.length === 1) r = '0'+r;
+	if(g.length === 1) g = '0'+g;
+	if(b.length === 1) b = '0'+b;
+
+	return ''+r+g+b;
+}
